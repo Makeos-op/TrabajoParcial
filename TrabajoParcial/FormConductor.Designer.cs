e930feb3f276e97971c7dd4cc07d514af5c8d61f
nosequepones7 <mauriciopezoj@gmail.com>
2025-10-24 17:00:03 -0500

    Puse los forms (conductor,vehiculo,arrendador y espacio)y creacion de los services

--- TrabajoParcial/FormConductor.Designer.cs
+++ TrabajoParcial/FormConductor.Designer.cs
@@ -28,37 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgConductor = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.tbApellidos = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbDNI = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Modelo = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.tbNacionalidad = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.tbCorreo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnVerVehiculo = new System.Windows.Forms.Button();
+            this.btnVerBrevete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgConductor)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgConductor
+            // 
+            this.dgConductor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgConductor.Location = new System.Drawing.Point(12, 186);
+            this.dgConductor.Name = "dgConductor";
+            this.dgConductor.RowHeadersWidth = 51;
+            this.dgConductor.RowTemplate.Height = 24;
+            this.dgConductor.Size = new System.Drawing.Size(768, 209);
+            this.dgConductor.TabIndex = 34;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(336, 132);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 33);
+            this.btnSalir.TabIndex = 33;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(12, 132);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 33);
+            this.btnRegistrar.TabIndex = 32;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.Location = new System.Drawing.Point(100, 78);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(117, 22);
+            this.tbApellidos.TabIndex = 31;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(100, 46);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(117, 22);
+            this.tbNombre.TabIndex = 30;
+            // 
+            // tbDNI
+            // 
+            this.tbDNI.Location = new System.Drawing.Point(108, 17);
+            this.tbDNI.Name = "tbDNI";
+            this.tbDNI.Size = new System.Drawing.Size(117, 22);
+            this.tbDNI.TabIndex = 29;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(108, -20);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(117, 22);
+            this.tbId.TabIndex = 28;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Apellidos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Nombre";
+            // 
+            // Modelo
+            // 
+            this.Modelo.AutoSize = true;
+            this.Modelo.Location = new System.Drawing.Point(18, 20);
+            this.Modelo.Name = "Modelo";
+            this.Modelo.Size = new System.Drawing.Size(30, 16);
+            this.Modelo.TabIndex = 25;
+            this.Modelo.Text = "DNI\r\n";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 42);
+            this.label1.Location = new System.Drawing.Point(26, -13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Id";
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Location = new System.Drawing.Point(336, 81);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(117, 22);
+            this.tbTelefono.TabIndex = 40;
+            // 
+            // tbNacionalidad
+            // 
+            this.tbNacionalidad.Location = new System.Drawing.Point(344, 46);
+            this.tbNacionalidad.Name = "tbNacionalidad";
+            this.tbNacionalidad.Size = new System.Drawing.Size(117, 22);
+            this.tbNacionalidad.TabIndex = 39;
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(344, 20);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(117, 22);
+            this.tbEdad.TabIndex = 38;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 96);
+            this.label2.Location = new System.Drawing.Point(254, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(242, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 16);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Nacionalidad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Edad";
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.Location = new System.Drawing.Point(584, 49);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.Size = new System.Drawing.Size(117, 22);
+            this.tbContraseña.TabIndex = 45;
+            // 
+            // tbCorreo
+            // 
+            this.tbCorreo.Location = new System.Drawing.Point(592, 20);
+            this.tbCorreo.Name = "tbCorreo";
+            this.tbCorreo.Size = new System.Drawing.Size(117, 22);
+            this.tbCorreo.TabIndex = 44;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(502, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 16);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Contraseña";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(502, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 16);
+            this.label9.TabIndex = 41;
+            this.label9.Text = "Correo";
+            // 
+            // btnVerVehiculo
+            // 
+            this.btnVerVehiculo.Location = new System.Drawing.Point(108, 132);
+            this.btnVerVehiculo.Name = "btnVerVehiculo";
+            this.btnVerVehiculo.Size = new System.Drawing.Size(94, 33);
+            this.btnVerVehiculo.TabIndex = 46;
+            this.btnVerVehiculo.Text = "Ver Vehiculo";
+            this.btnVerVehiculo.UseVisualStyleBackColor = true;
+            // 
+            // btnVerBrevete
+            // 
+            this.btnVerBrevete.Location = new System.Drawing.Point(219, 132);
+            this.btnVerBrevete.Name = "btnVerBrevete";
+            this.btnVerBrevete.Size = new System.Drawing.Size(86, 33);
+            this.btnVerBrevete.TabIndex = 47;
+            this.btnVerBrevete.Text = "Ver Brevete";
+            this.btnVerBrevete.UseVisualStyleBackColor = true;
             // 
             // FormConductor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(768, 331);
+            this.ClientSize = new System.Drawing.Size(792, 407);
+            this.Controls.Add(this.btnVerBrevete);
+            this.Controls.Add(this.btnVerVehiculo);
+            this.Controls.Add(this.tbContraseña);
+            this.Controls.Add(this.tbCorreo);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbNacionalidad);
+            this.Controls.Add(this.tbEdad);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgConductor);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbDNI);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Modelo);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormConductor";
             this.Text = "FormConductor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgConductor)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +283,28 @@
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dgConductor;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox tbApellidos;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbDNI;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Modelo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.TextBox tbNacionalidad;
+        private System.Windows.Forms.TextBox tbEdad;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbContraseña;
+        private System.Windows.Forms.TextBox tbCorreo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnVerVehiculo;
+        private System.Windows.Forms.Button btnVerBrevete;
     }
 }
